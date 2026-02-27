6c356b6b841db86514900c29da4326df31cab974
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-27 10:09:30 -0300

    feat: adiciona métodos de comportamento e atualiza README

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -5,17 +5,22 @@ import br.com.fiapride.model.Humano;
 public class SistemaPrincipal {
 
 	public static void main(String[] args) {
-		Humano Isabella = new Humano();
+		Humano Isabella = new Humano(null, 0, null);
 		Isabella.faculdade = "FIAP";
 		Isabella.idade = 20;
 		Isabella.sexo = "Feminino";
+		Isabella.catracaFiap(Isabella.faculdade);
 		
-		Humano Matheus = new Humano();
+		Humano Matheus = new Humano(null, 0, null);
 		Matheus.faculdade = "UniNassau";
 		Matheus.idade = 26;
+		Matheus.catracaFiap(Matheus.faculdade);
 		
 		System.out.println("Minha faculdade (Isabella) é: " + Isabella.faculdade);
 		System.out.println("A do Matheus é: " + Matheus.faculdade);
 		System.out.println("Eu nasci em: " + (2025 - Isabella.idade) + " Ele nasceu em: " + (2025 - Matheus.idade));
+		
+		Isabella.fezAniversario(Isabella.idade);
+		Matheus.fezAniversario(Matheus.idade);
 	}
 }
--- src/br/com/fiapride/model/Humano.java
+++ src/br/com/fiapride/model/Humano.java
@@ -1,7 +1,28 @@
 package br.com.fiapride.model;
+
 public class Humano {   
     public String faculdade;
     public int idade;
     public String sexo;
     
+
+	public Humano(String faculdade, int idade, String sexo) {
+	    this.faculdade = faculdade;
+	    this.idade = idade; // Novo Atributo
+	    this.sexo = sexo;
+	}
+	
+	public void catracaFiap(String faculdade) {
+	    if (faculdade == "FIAP") {
+	        System.out.println("Acesso autorizado.");
+	        return;
+	    } else {
+	    	System.out.println("Acesso negado");
+	    } 
+	}
+
+	public void fezAniversario(int idade) {
+		this.idade += 1;
+		System.out.println("O usuário fez aniversário, agora a idade dele é " + this.idade);
+	}
 }
