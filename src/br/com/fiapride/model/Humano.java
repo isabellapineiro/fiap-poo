b6e72aaef8fb588c76a1af4ab57120b1777e8e36
Isabella Piñeiro Santana <114621531+isabellapineiro@users.noreply.github.com>
2026-03-09 20:28:43 -0300

    Update Humano.java

--- src/br/com/fiapride/model/Humano.java
+++ src/br/com/fiapride/model/Humano.java
@@ -1,6 +1,5 @@
 package br.com.fiapride.model;
 
-<<<<<<< HEAD
 public class Humano {
 
     private String faculdade;
@@ -14,7 +13,6 @@ public class Humano {
     }
 
     // Getters
-
     public String getFaculdade() {
         return faculdade;
     }
@@ -28,12 +26,11 @@ public class Humano {
     }
 
     // Setters
-
     public void setFaculdade(String faculdade) {
         this.faculdade = faculdade;
     }
 
-    // REGRA: idade não pode ser negativa
+    // regra: idade não pode ser negativa
     public void setIdade(int idade) {
         if (idade >= 0) {
             this.idade = idade;
@@ -58,31 +55,4 @@ public class Humano {
         this.idade += 1;
         System.out.println("O usuário fez aniversário, agora a idade dele é " + this.idade);
     }
-=======
-public class Humano {   
-    public String faculdade;
-    public int idade;
-    public String sexo;
-    
-
-	public Humano(String faculdade, int idade, String sexo) {
-	    this.faculdade = faculdade;
-	    this.idade = idade; // Novo Atributo
-	    this.sexo = sexo;
-	}
-	
-	public void catracaFiap(String faculdade) {
-	    if (faculdade == "FIAP") {
-	        System.out.println("Acesso autorizado.");
-	        return;
-	    } else {
-	    	System.out.println("Acesso negado");
-	    } 
-	}
-
-	public void fezAniversario(int idade) {
-		this.idade += 1;
-		System.out.println("O usuário fez aniversário, agora a idade dele é " + this.idade);
-	}
->>>>>>> 6c356b6b841db86514900c29da4326df31cab974
 }
